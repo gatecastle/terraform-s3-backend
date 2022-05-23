99be65ef7dbfda45d7267ff0ee90f97739e1ffd7
Greg Nagy <greg@gatecastle.com>
2022-05-23 19:40:45 +0200

    add provider
    Signed-off-by: Greg Nagy <greg@gatecastle.com>

--- provider.tf
+++ provider.tf
@@ -1,12 +1,11 @@
-terraform {
-  required_providers {
-    aws = {
-      source  = "hashicorp/aws"
-      version = "~> 3.0"
-    }
-  }
-}
-
 provider "aws" {
-  region = var.common_config.region
+  # The security credentials for AWS Account A.
+  access_key = "{{ access_key }}"
+  secret_key = "{{ secret_key }}"
+  region     = "{{ region  }}"
+
+  assume_role {
+    # The role ARN within Account B to AssumeRole into. Created in step 1.
+    role_arn    = "arn:aws:iam::{{ account }}:role/{{ role }}"
+  }
 }
