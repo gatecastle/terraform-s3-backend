3b1a5945ad8cfa12ea9ec76b415c7a8443979057
Greg Nagy <greg@gatecastle.com>
2022-05-23 21:35:26 +0200

    provider
    Signed-off-by: Greg Nagy <greg@gatecastle.com>

--- provider.tf
+++ provider.tf
@@ -1,3 +1,12 @@
+terraform {
+  required_providers {
+    aws = {
+            source  = "hashicorp/aws"
+                  version = "~> 3.0"
+    }
+  }
+}
+
 provider "aws" {
   # The security credentials for AWS Account A.
   access_key = "{{ access_key }}"
